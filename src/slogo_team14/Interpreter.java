3d69b3dbdbddead4e08efa9ba15c11988edc62f4
Collin Brown <cdb55@duke.edu>
2018-02-22 17:47:56 -0500

    added error checking for interpret method

--- src/slogo_team14/Interpreter.java
+++ src/slogo_team14/Interpreter.java
@@ -4,14 +4,29 @@ import java.io.File;
 import java.util.Properties;
 
 public class Interpreter {
-	private String myInput;
 	private sLogoValid mySlogoValid;
 	private Properties myLanguageProperties; 
 	
-	public Interpreter(String language, String input) {
-		myInput = input;
-		myLanguageProperties = new languageParser(language).getProperties();
+	
+	public Interpreter(String language) {
+		mySlogoValid = new sLogoValid();
+		//Try to import the language properties
+		try {
+			myLanguageProperties = new languageParser(language).getProperties();
+		} catch(Exception e) {
+			mySlogoValid.setError(true);
+			mySlogoValid.setMyStringValue("Error: Can not find " + language + ".properties");
+		}
 		
+	}
+	
+	public sLogoValid interpret(String s) {
+		//Check to ensure there aren't prior errors
+		if(mySlogoValid.getError()) {
+			return mySlogoValid;
+		} 
+		sLogoValid tempSlogoValid = new sLogoValid();
+		return tempSlogoValid;
 	}
 	
 }
