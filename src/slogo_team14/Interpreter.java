2356441196b3b49b44c8191c663f540ed4c526f8
Collin Brown <cdb55@duke.edu>
2018-02-22 17:15:37 -0500

    created languageParser class

---
+++ src/slogo_team14/Interpreter.java
@@ -0,0 +1,12 @@
+package slogo_team14;
+
+import java.io.File;
+
+public class Interpreter {
+	private String myInput;
+	private File myLanguageFile;
+	public Interpreter(String language, String input) {
+		myInput = input;
+	}
+	
+}
---
+++ src/slogo_team14/languageParser.java
@@ -0,0 +1,23 @@
+package slogo_team14;
+
+import java.io.PrintStream;
+import java.util.Properties;
+
+public class languageParser {
+	private Properties myLanguageProperties = new Properties();
+	
+	public languageParser(String lang) {
+	try {
+		myLanguageProperties.load(this.getClass().getResourceAsStream("/resources/languages/"+ lang + ".properties"));
+	} catch(Exception e){
+		System.out.println("Error Parsing in language file");
+		e.printStackTrace();
+	}
+	}
+
+
+	//public static void main(String[] args) {
+	//	languageParser lp = new languageParser("English");
+	//}
+
+}
