33963f2c65275513a097e82f9638fe9e05b27b6e
Marcus Oertle <mlo11@duke.edu>
2018-02-24 17:56:01 -0500

    updated TurtleDisplayer and Console, can now display turtle and has working command prompt

--- src/frontend/components/Console.java
+++ src/frontend/components/Console.java
@@ -1,24 +1,90 @@
 package frontend.components;
 
+import java.util.Map;
+import java.util.Set;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
 public class Console implements ComponentBuilder{
+	private static final double BUTTON_SIZE = 50;
 	
 	private VBox box = new VBox();
+	private String commands;
+	private TextArea prompt = new TextArea();
 	
 	public Console () {
 		box.setStyle("-fx-background-color: #7777FF;");
 		Label label = new Label("Console");
-		label.setPrefSize(5000, 200);
-		box.getChildren().add(label);
+		label.setPrefSize(100, 5);
+		
+		Button runButton = makeGetTextButton();
+		Button clearButton = makeClearButton();
+		HBox hbox = new HBox(clearButton, runButton);
+		
+		prompt.setStyle("-fx-control-inner-background:#000000; -fx-text-fill: #FFFFFF;");
+			    
+		box.getChildren().addAll(label, prompt, hbox);
+		
 	}
 	@Override
 	public Node getNode() {
 		// TODO Auto-generated method stub
 		return box;
 	}
-	
+		
+	private Button makeGetTextButton() {
+		Button runButton = new Button("Run");
+		runButton.setMinWidth(BUTTON_SIZE);
 
+		runButton.setOnAction(action -> {
+			//run();
+			commands = prompt.getText();
+			System.out.println(commands);
+        });
+
+        return runButton;
+	}
+	
+	private Button makeClearButton() {
+		Button runButton = new Button("Clear");
+		runButton.setMinWidth(BUTTON_SIZE);
+
+		runButton.setOnAction(action -> {
+			prompt.clear();
+        });
+
+        return runButton;
+	}
+	
+	public void run(){
+		commands = prompt.getText();
+	    SLogoValid retMessage = controller.interpret(commands);
+	    boolean isValid = retMessage.isValid();
+	    if(isValid){
+	        Map<String, Object> variableMap = ModelViewable.getCurrentVariables();
+	        Set<Turtle> turtleSet = new Set<Turtle>
+	        for(String s : variableMap.keys()){
+	            if(variableMap.get(s) == instanceof(Turtle)){
+	                turtleSet.add(variableMap.get(s));
+	            }
+	        }
+	        if(!turtleSet.isEmpty()){
+	            TurtleDisplayer.draw(turtleSet);
+	        }  
+	    }
+	    else{
+	        ErrorPrinter(retMessage.toString);
+	    }
+	}
 }
--- src/frontend/components/TurtleDisplayer.java
+++ src/frontend/components/TurtleDisplayer.java
@@ -1,19 +1,106 @@
 package frontend.components;
 
+import java.util.ArrayList;
+import java.util.Set;
+
+import frontend.IDEBuilder;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
+import javafx.util.Duration;
 
 public class TurtleDisplayer implements ComponentBuilder{
+	private static final String TURTLE_IMAGE = "turtleScaled.png";
+	private static final double TURTLE_SIZE = 50;
+	private static final double DEFAULT_WINDOW_X = 400;
+	private static final double DEFAULT_WINDOW_Y = 560;
+	private static final int FRAMES_PER_SECOND = 60;
+	private static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
+	private static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
 	
-	private Group group = new Group();
+	private Pane group = new Pane();
+	private ArrayList<ImageView> viewables = new ArrayList<ImageView>();
+	private double prev_center_x = DEFAULT_WINDOW_X/2-TURTLE_SIZE/2;
+	private double prev_center_y = DEFAULT_WINDOW_Y/2-TURTLE_SIZE/2;
+	private double center_x;
+	private double center_y;
 	
 	public TurtleDisplayer() {
 		group.setStyle("-fx-background-color: #9999FF;");
-		group.getChildren().add(new Label("Turtle Display"));
+		ImageView turtleView = drawTurtle(DEFAULT_WINDOW_X/2, DEFAULT_WINDOW_Y/2, 0);
+		group.getChildren().addAll(turtleView);
+
+		// animation if needed to scale UI, delete otherwise
+		KeyFrame frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY),
+				e -> step(SECOND_DELAY));
+		Timeline animation = new Timeline();
+		animation.setCycleCount(Timeline.INDEFINITE);
+		animation.getKeyFrames().add(frame);
+		animation.play();
+		
 	}
 
 	public Node getNode() {
 		return group;
 	}
+	
+	public void draw(Set<Turtle> s){
+	    eraseCurrentDisplay();
+	    drawNewDisplay(s);
+	}
+
+	private void step(double elapsedTime) {
+		// if need to update UI to scale, use this
+		center_x = group.getBoundsInLocal().getMaxX()/2 - TURTLE_SIZE/2;
+		center_y = group.getBoundsInLocal().getMaxY()/2 - TURTLE_SIZE/2;
+		double xRatio = center_x/prev_center_x;
+		double yRatio = center_y / prev_center_y;
+		for(ImageView i : viewables) {
+			i.setX(i.getX()*xRatio);
+			i.setY(i.getY()*yRatio);
+		}
+		prev_center_x = center_x;
+		prev_center_y = center_y;
+	}
+	
+	private void eraseCurrentDisplay(){
+		viewables.clear();
+	    group.getChildren().clear();
+	}
+
+	private void drawNewDisplay(Set<Turtle> s){
+	    for(Turtle t : s){
+	        drawTurtle(t.getX(), t.getY(), t.getAngle());
+	        if(!t.getLinesSet.isEmpty()){
+	            drawLines(t.getLinesSet);
+	        }
+	    }
+	}
+
+	private void drawLines(Set<Line> lines) {
+		for(Line l : lines) {
+			group.getChildren().add(l);
+		}		
+	}
+	
+	private ImageView drawTurtle(double x, double y, double angle) {
+		Image turtleImage = new Image(getClass().getClassLoader().getResourceAsStream(TURTLE_IMAGE));
+		ImageView turtleView = new ImageView(turtleImage);
+		turtleView.setX(x-TURTLE_SIZE/2);
+		turtleView.setY(y-TURTLE_SIZE/2);
+		turtleView.setRotate(angle);
+		//System.out.println(turtleView.getX());
+		//System.out.println(turtleView.getY());
+		viewables.add(turtleView);
+		return turtleView;
+	}
+	
 }
