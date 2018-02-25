fb013ba561936847f37b8293b616c3fbd0ea7ceb
Collin Brown <cdb55@duke.edu>
2018-02-25 03:13:46 -0500

    working on the argument check method

--- src/slogo_team14/Interpreter.java
+++ src/slogo_team14/Interpreter.java
@@ -1,17 +1,15 @@
 package slogo_team14;
 
-import java.io.File;
 import java.util.Properties;
 
 public class Interpreter {
-	private String myLanguage;
 	private sLogoValid mySlogoValid;
 	private Properties myLanguageProperties;
-	
+	private static final String[] noParamCommands = {"PenUp","PenDown","ShowTurtle","HideTurtle","Home","ClearScreen","XCoordinate","YCoordinate","Heading","IsPenDown","IsShowing","Pi"};
+	private static final String[] oneParamCommands = {"Forward", "Backward", "Left", "Right", "SetHeading", "Random", "Sine", "Cosine", "Tangent", "ArcTangent", "NaturalLog", "Not" };
 	
 	
 	public Interpreter(String language) {
-		myLanguage = language;
 		mySlogoValid = new sLogoValid();
 		//Try to import the language properties
 		try {
@@ -42,16 +40,48 @@ public class Interpreter {
 			tempSlogoValid.setMyStringValue("Invalid command: " + args[0]);
 			tempSlogoValid.setError(true);
 			return tempSlogoValid;
-		} else {
-			tempSlogoValid.setMyStringValue(myLanguageProperties.getProperty(args[0]));
-		}
-		
+		} 
+		String myCommand = myLanguageProperties.getProperty(args[0]);
+		String[] mySyntax = getCommandSyntax(myCommand);
+		//Handles if there is no argument needed
+		tempSlogoValid = argumentCheck(args, mySyntax);
+		//tempSlogoValid.setMyStringValue(myCommand); //TODO: replace this with something that makes a command object
+				
 		return tempSlogoValid;
 	}
-	
+	private sLogoValid argumentCheck(String[] args, String[] expectedArguments) {
+		sLogoValid tempSlogoValid = new sLogoValid();
+		int expectedArgsNum = expectedArguments.length;
+			if(args.length > expectedArgsNum+1) {
+				String myConcatArgs = "";
+				for(int i = expectedArgsNum+1; i < args.length; i++) {
+					myConcatArgs += args[i] + " ";
+				}
+				tempSlogoValid = interpret(myConcatArgs);
+				//TODO: find a way to pass this up a level or print it to the prompt
+			}
+			tempSlogoValid.setMyStringValue(myLanguageProperties.getProperty(args[0]));
+		return tempSlogoValid;	
+	}
+	private String[] getCommandSyntax(String myCommand) {
+		for(String k : noParamCommands) {
+			if(myCommand.equals(k)) {
+				return new String[0];
+			}
+		}
+		for(String k : oneParamCommands) {
+			if(myCommand.equals(k)) {
+				return new String[1];
+			}
+		}
+		return null;
+	}
+
 	public static void main(String[] args) {
-		Interpreter i = new Interpreter("French");
-		sLogoValid s = i.interpret("dev"); 
+		Interpreter i = new Interpreter("English");
+		sLogoValid s = i.interpret("pu");  
+		System.out.println(s.getMyStringValue());
+		s = i.interpret("fd 20");  
 		System.out.println(s.getMyStringValue());
 		
 	}
