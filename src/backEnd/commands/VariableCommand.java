51911b54d0c4c539837e2fe6e059cbddfad5aa60
Scott Pledger <stp20@duke.edu>
2018-02-27 16:12:25 -0500

    Variable Command progress

--- src/backEnd/commands/VariableCommand.java
+++ src/backEnd/commands/VariableCommand.java
@@ -1,10 +1,29 @@
 package backEnd.commands;
 
+import backEnd.sLogoValid;
+
 public class VariableCommand extends CommandGroup {
+	private ModelModifiable myModel;
 
 	public VariableCommand(String command, String[] args) {
 		super(command, args);
+		myModel = new Model();
+		this.mySlogoValid = run(command, args);
 		// TODO Auto-generated constructor stub
 	}
+	
+	private sLogoValid run(String command, String[] args){
+		if (command.equals("make") || command.equals("set")){
+			return setVar(command, args);
+		}
+		return null;
+	}
+	
+	private sLogoValid setVar(String command, String[] args){
+		
+		return null;
+	}
+	
+	
 
 }
