082f326dc662888bfad983b781a9a47ef3d8545e
Trishul Nagenalli <tn74@duke.edu>
2018-02-27 21:28:37 -0500

    Added methods for language picker in toolbar

--- src/frontend/components/ToolBar.java
+++ src/frontend/components/ToolBar.java
@@ -1,18 +1,25 @@
 package frontend.components;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import frontend.IDEBuilder;
+import javafx.collections.ObservableList;
 import javafx.scene.Node;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.HBox;
 
 public class ToolBar implements ComponentBuilder{
 	private HBox bar = new HBox();
 	private IDEBuilder builder;
-	
+	String[] languages = {"English", "German", "French", "Spanish"};
+	private ComboBox languagePicker = new ComboBox((ObservableList)Arrays.asList(languages));
+	private Button updateEnvButton = new Button("Update");
 	public ToolBar(IDEBuilder b) {
 		builder = b;
 		bar.setStyle("-fx-background-color: #FF9999;");
-		bar.getChildren().add(new Label("Toolbar"));
 		bar.setPrefHeight(IDEBuilder.TOOLBAR_HEIGHT);
 	}
 	
@@ -23,4 +30,15 @@ public class ToolBar implements ComponentBuilder{
 	public String getLanguage() {
 		return "English";
 	}
+	
+	public void addComponents() {
+		List<Node> toAdd = new ArrayList<>();
+		
+		toAdd.add(languagePicker);
+		
+		updateEnvButton.setOnAction(e -> builder.update());
+		toAdd.add(updateEnvButton);
+	}
+	
+	
 }
